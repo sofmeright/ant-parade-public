744f795f58b6d9233dc1172e11b2f074befe7a0f
Kai H <kai.hamil@gmail.com>
2025-05-04 05:53:02 +0000

    Add new file

---
+++ maintenance/migrate_monitoring_data.sh
@@ -0,0 +1,2 @@
+# This script pulls data from the source to the new monitoring stack location.
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/grafana /mnt/app_data/Server/_docker_stack/grafana
