770c7ad2c8318f4ff72a4beb70cef2b1b62f1d8c
Kai H <kai.hamil@gmail.com>
2025-05-04 05:58:43 +0000

    Edit migrate_monitoring_data.sh

--- maintenance/migrate_monitoring_data.sh
+++ maintenance/migrate_monitoring_data.sh
@@ -1,2 +1,11 @@
 # This script pulls data from the source to the new monitoring stack location.
 sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/grafana /mnt/app_data/Server/_docker_stack/grafana
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/graphite-exporter /mnt/app_data/Server/_docker_stack/graphite-exporter
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/loki /mnt/app_data/Server/_docker_stack/loki
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/ntfy /mnt/app_data/Server/_docker_stack/ntfy
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/prometheus /mnt/app_data/Server/_docker_stack/prometheus
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/prometheus-eaton-ups-exporter /mnt/app_data/Server/_docker_stack/prometheus-eaton-ups-exporter
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/prometheus-pve-exporter /mnt/app_data/Server/_docker_stack/prometheus-pve-exporter
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/promtail /mnt/app_data/Server/_docker_stack/promtail
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/uptime-kuma /mnt/app_data/Server/_docker_stack/uptime-kuma
+sudo rsync -avz kai@moor:/mnt/app_data/Server/_docker_stack/wazuh /mnt/app_data/Server/_docker_stack/wazuh
